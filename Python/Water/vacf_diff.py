355574b10ccf68e1ebb67be5f48eeba6bc3e0eb6
Frederike Jaeger <frederike.jaeger@gmail.com>
2017-03-01 15:23:48 +0000

    Working VACF script for per atom property

--- Python/Water/vacf_diff.py
+++ Python/Water/vacf_diff.py
@@ -7,9 +7,9 @@ import matplotlib
 import argparse
 from scipy.integrate import simps
 
-def data(A_dict):
+def read_data():
     print 'Reading in data...'
-    filename = "pair.local"
+    filename = "vel.dat"
     
     # read in data
     f = open(filename,'r')
@@ -17,123 +17,76 @@ def data(A_dict):
     
     data_lines = data.split('\n')
     
-    type1 = []
-    type2 = []
-    Int = []
-    Dist = []
-    Dist2 = []
-    eV2J = 1.602176565e-19
+    data_list = []
+    data_tmp = []
+
     A2m = 1e-10
+    ps2s = 1e-12
 
     Natoms = 0
     count = 0
     for i in range(9,len(data_lines)-1): 
-        if count < 100:     
+        if count < 1000:     
             try:
                 if data_lines[i]=='ITEM: TIMESTEP':
+                    if data_tmp != 0:
+                        data_list.append(np.array(data_tmp))
+                    data_tmp = []
                     count += 1
-                    print 'Reading in timestep:', count
-                type1 = data_lines[i].split()[1]
-                type2 = data_lines[i].split()[2]
-                Dist.append(float(data_lines[i].split()[3]))
-                if (A_dict[type1]=='C' and A_dict[type2] == 'O') or (A_dict[type1]=='O' and A_dict[type2] == 'C'):
-                    Dist2.append(float(data_lines[i].split()[3])*A2m)
-                    Int.append(float(data_lines[i].split()[4])*eV2J)
+                    #print 'Reading in timestep:', count
+                #if statement for coords
+                if len(data_lines[i].split()) == 8:
                     Natoms += 1
+                    vx = float(data_lines[i].split()[5])*A2m/ps2s
+                    vy = float(data_lines[i].split()[6])*A2m/ps2s
+                    vz = float(data_lines[i].split()[7])*A2m/ps2s
+                    vel = np.array([vx, vy, vz])
+                    data_tmp.append(vel)
             except:
                 continue
 
-    
-    Eint = sum(Int)/count
-    print 'Finished reading in data. %i data points collected.' % Natoms
+    data_list = np.array(data_list)
+    print 'Read in', count, 'timesteps.'
+
+    return data_list
+
+def correlation(data):
+    vacf = []
+    times_range = 40
+    print 'Calculating the autocorrelation function for', times_range*100*0.0005, 'ps.'
+    for i in range(data.shape[0]-times_range):
+        Cdata = []
+        for j in range(times_range+1):
+            Cdelta = []
+            C_tmp = 0
+            count = 0
+            for k in range(data[i].shape[0]):
+                count += 1
+                C_tmp += np.dot(data[i][k], data[i+j][k])
+                
+            C_tmp = C_tmp/count
+            Cdata.append(C_tmp)
+        vacf.append(Cdata)
+    vacf = np.array(vacf)
+    mean_vacf = np.mean(vacf, axis=0)
+    norm_vacf = mean_vacf/mean_vacf[0]
+    print vacf
+    print mean_vacf
+    print norm_vacf
+
+    return norm_vacf
+
+def time_data(Cdata):
+    print 'Creating corresponding time array.'
+    times = []
+    # get corresponding timesteps
+    for i in range(len(Cdata)):
+        t_tmp = i*100*0.0005 # data sampled every 100, dt=0.0005ps
+        times.append(t_tmp)
+
+    return times
 
 
-    return Int, Eint, Dist2, count
-
-def Atom_lookup2():
-    filename = "data.graphene"
-    # read in data
-    f = open(filename,'r')
-    data = f.read()
-
-    atom_dict = {}
-    
-    data_lines = data.split('\n')
-    blank_count = 0
-
-    for i in range(0,len(data_lines)-1):
-        if blank_count == 5 and len(data_lines[i].split()) > 1:
-            atom_dict['%s'%data_lines[i].split()[0]] = data_lines[i].split()[2]
-        if data_lines[i].split() == []:
-            blank_count +=1
-
-    return atom_dict
-
-def Atom_lookup():
-    filename = "water.xyz"
-    # read in data
-    f = open(filename,'r')
-    data = f.read()
-
-    atom_dict = {}
-    
-    data_lines = data.split('\n')
-    count = 0
-    atom_count = 0
-
-    for i in range(0,len(data_lines)-1):
-        if count == 2 and len(data_lines[i].split()) > 1:
-            atom_count += 1
-            atom_dict['%i'%atom_count] = data_lines[i].split()[0]
-        if data_lines[i].split()[0]!= ('C' or 'H' or 'O'):
-            count +=1
-
-    return atom_dict
-
-def Bins(dist, pot, no_bins):
-    dist = np.array(dist)
-    pot = np.array(pot)
-    hist, bins = np.histogram(dist, bins=no_bins, density=False)
-
-    # Find number density
-    density = []
-    for i in range(len(hist)):
-        density.append(hist[i]/(bins[i+1]-bins[i]))
-
-    
-
-    regular_dist = bins # Regular heights every 100m
-    regular_pot = []
-
-    # Find average potential values
-    for i in range(len(regular_dist)-1):
-        mask = np.logical_and(dist > regular_dist[i], dist < regular_dist[i+1])
-        mean = np.mean(pot[mask])
-        regular_pot.append(mean)
-
-    regular_pot = np.hstack((regular_pot))
-
-    # find average bin values
-    avg_bins = []
-    for i in range(len(bins)-1):
-        avg_bins.append((bins[i+1]+bins[i])/2)
-
-    return density, avg_bins, regular_pot
-
-def Area():
-
-    filename = "data.graphene"#"graphene_slit_9_9_40_1.20.dat"
-    # read in data
-    f = open(filename,'r')
-    data = f.read()
-    
-    data_lines = data.split('\n')
-    xlo, xhi = float(data_lines[11].split()[0]), float(data_lines[11].split()[1])
-    ylo, yhi = float(data_lines[12].split()[0]), float(data_lines[12].split()[1])
-
-    A = (xhi-xlo)*(yhi-ylo)*10**(-20) # in m^2
-
-    return A
 
 def find_region(dist, pot):
     dist, pot = np.array(dist), np.array(pot)
@@ -146,45 +99,17 @@ def find_region(dist, pot):
 
 
 def main():
-    print 'Creating atom type dictionary'
-    Atom_dict = Atom_lookup()
-    Int, Eint, Dist, ts = data(Atom_dict)
-    A = Area()
-    dens, bins, pot = Bins(Dist, Int, 100)
+    DAT = read_data()
+    CDAT = correlation(DAT)
+    TDAT = time_data(CDAT)
 
-    Eint_layer, int_layer, dist_layer = find_region(Dist, Int)
-    dens_layer, bins_layer, pot_layer = Bins(dist_layer, int_layer, 20)
-
-    WA = Eint/(2*A)
-    WA_layer = Eint_layer/(2*A)
-    print 'The work of adhesion is: ', WA
-    print 'The work of adhesion in the layer is: ', WA_layer
-
-    # integrate V(z)*n(z)
-    Vn = [] 
-    Vn_layer = []
-    for i in range(len(pot)):
-        Vn.append(pot[i]*dens[i])
-    WA_int = simps(Vn, bins)/(2*ts*A)
-
-    for i in range(len(pot_layer)):
-        Vn_layer.append(pot_layer[i]*dens_layer[i])
-    WA_int_layer = simps(Vn_layer, bins_layer)/(2*ts*A)
-
-    print 'The integrated value of WA is:', WA_int
-    print 'The integrated value of WA in the layer is:', WA_int_layer
+    plt.figure()
+    plt.plot(TDAT, CDAT)
+    plt.show()
 
 
-    plt.plot(bins, Vn)
-    #plt.show()
 
-    plt.plot(Dist, Int, linestyle='None', marker='x')
-    plt.plot(bins, pot, linestyle='None', marker='D')
-    #plt.show()
-
-    plt.plot(bins, dens, linestyle='None', marker='x')
-    #plt.show()
-
+    return
 
 
 if __name__ == "__main__":
